e51a2268d9a3ab588d60ffad2f37b00a96848f2c
Katia Bazzi <katia@eventbrite.com>
2015-11-05 11:43:07 -0800

    boiler plate

---
+++ Bring Me Fries/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bring Me Fries
+//
+//  Created by katia on 11/5/15.
+//  Copyright (c) 2015 katia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Bring Me FriesTests/Bring_Me_FriesTests.swift
@@ -0,0 +1,36 @@
+//
+//  Bring_Me_FriesTests.swift
+//  Bring Me FriesTests
+//
+//  Created by katia on 11/5/15.
+//  Copyright (c) 2015 katia. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Bring_Me_FriesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
